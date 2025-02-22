a29714d9f92cf49ca997bd24f7d3fb6fdb46434b
thompsryann <ryan.thompson@vuw.ac.nz>
2025-02-22 03:00:31 +0000

    Deploying to gh-pages from @ thompsryann/thompsryann.github.io@58246d933dfe6b2dbd956da0de170afb4ad5deda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "Click here for a full copy of my CV.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "news-a-simple-inline-announcement",
+  },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
