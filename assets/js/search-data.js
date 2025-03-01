ee537d43599f2bbd2434b6b5472b1f02a89699ff
thompsryann <ryan.thompson@vuw.ac.nz>
2025-03-01 03:15:41 +0000

    Deploying to gh-pages from @ thompsryann/homepage@805ee703af6f731177b3769e2c8948e11330825f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,18 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-first-post",
-      
-        title: "First Post",
-      
-      description: "Groupoids",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/testing/";
-        
-      },
-    },{id: "news-a-simple-inline-announcement",
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
